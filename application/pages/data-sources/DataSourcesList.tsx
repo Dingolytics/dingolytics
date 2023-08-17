ab1bb019ac34821911375d75c14fdda38032c71e
Alexey Kinev <rudy@05bit.com>
2023-08-17 14:59:49 +0400

    Remove streams from data sources page

--- application/pages/data-sources/DataSourcesList.tsx
+++ application/pages/data-sources/DataSourcesList.tsx
@@ -2,24 +2,20 @@ import { isEmpty, reject } from "@lodash";
 import React, { Component } from "react";
 import { Button, Space, Table, Col, Row, Typography } from "antd";
 import type { ColumnsType } from "antd/es/table";
-const { Title, Text } = Typography;
+const { Text } = Typography;
 
 import DataSource, { DataSourceType } from "@/services/data-source";
-import { annotateWithStreams, IMG_ROOT } from "@/services/data-source";
-import Stream, { StreamType } from "@/services/stream";
+import { IMG_ROOT } from "@/services/data-source";
 import { policy } from "@/services/policy";
 import recordEvent from "@/services/recordEvent";
 import routes from "@/services/routes";
 
-import Link from "@/components/general/Link";
 import routeWithUserSession from "@/components/router/routeWithUserSession";
 import navigateTo from "@/components/router/navigateTo";
 import LoadingState from "@/components/items-list/components/LoadingState";
 import CreateSourceDialog from "@/components/settings/CreateSourceDialog";
-import helper from "@/components/dynamic-form/dynamicFormHelper";
+import dynamicFormHelper from "@/components/dynamic-form/dynamicFormHelper";
 import wrapSettingsTab from "@/components/settings/SettingsWrapper";
-import CreateStreamDialog from "@/components/streams/CreateStreamDialog";
-import { GrouppedStreamsList} from "@/components/streams/StreamsList";
 import DatabaseItem from "@/components/databases/DatabaseItem";
 
 type ListComponentProps = {
@@ -34,7 +30,6 @@ type DataSourcesPageProps = {
 type DataSourcesPageState = {
   dataSourceTypes: any[];
   dataSources: any[];
-  streams: StreamType[];
   loading: boolean;
 }
 
@@ -74,11 +69,8 @@ class DataSourcesList extends Component<DataSourcesPageProps, DataSourcesPageSta
   state: DataSourcesPageState = {
     dataSourceTypes: [],
     dataSources: [],
-    streams: [],
     loading: true,
   };
-
-  private streamDialog: any = null;
 
   private databaseDialog: any = null;
 
@@ -90,19 +82,14 @@ class DataSourcesList extends Component<DataSourcesPageProps, DataSourcesPageSta
     if (this.databaseDialog) {
       this.databaseDialog.dismiss();
     }
-
-    if (this.streamDialog) {
-      this.streamDialog.dismiss();
-    }
   }
 
   reloadState = () => {
-    Promise.all([DataSource.query(), DataSource.types(), Stream.query()])
+    Promise.all([DataSource.query(), DataSource.types()])
       .then((values) => {
-        let [dataSources, dataSourceTypes, streams] = values;
-        annotateWithStreams(dataSources, streams);
+        let [dataSources, dataSourceTypes] = values;
         this.setState(
-          { dataSources, dataSourceTypes, streams, loading: false },
+          { dataSources, dataSourceTypes, loading: false },
           () => {
             if (this.props.isNewDataSourcePage) {
               if (policy.canCreateDataSource()) {
@@ -119,7 +106,7 @@ class DataSourcesList extends Component<DataSourcesPageProps, DataSourcesPageSta
 
   createDataSource = (selectedType: any, values: any): Promise<any> => {
     const target = { options: {}, type: selectedType.type, name: values.name };
-    helper.updateTargetWithValues(target, values);
+    dynamicFormHelper.updateTargetWithValues(target, values);
     return DataSource.create(target).then((dataSource) => {
       this.setState({ loading: true });
       DataSource.query()
@@ -154,24 +141,6 @@ class DataSourcesList extends Component<DataSourcesPageProps, DataSourcesPageSta
       });
   };
 
-  showCreateStreamDialog = () => {
-    const dialog = CreateStreamDialog.showModal({});
-
-    dialog.onClose((result = {success: false, data: {id: null}}) => {
-      this.streamDialog = null;
-      if (result.success) {
-        this.reloadState();
-      }
-    });
-
-    dialog.onDismiss(() => {
-      this.streamDialog = null;
-      navigateTo("data-sources", true);
-    });
-
-    this.streamDialog = dialog;
-  }
-
   render() {
     const allowCreation = policy.isCreateDataSourceEnabled();
 
@@ -199,36 +168,6 @@ class DataSourcesList extends Component<DataSourcesPageProps, DataSourcesPageSta
             }
           >
             Connect a Database
-          </Button>
-        </Space>
-
-        <Title level={4}>
-          Data streams
-        </Title>
-
-        {
-          this.state.loading ? (
-            <LoadingState className="" />
-          ) : (
-            <Row>
-              <Col span={24} lg={{span: 16}}>
-                <GrouppedStreamsList
-                  items={this.state.dataSources}
-                />
-              </Col>
-            </Row>
-          )
-        }
-
-        <Space style={{marginTop: "1rem"}}>
-          <Button
-            type="primary"
-            disabled={!allowCreation}
-            onClick={
-              allowCreation ? this.showCreateStreamDialog : undefined
-            }
-          >
-            Add Stream
           </Button>
         </Space>
       </div>
