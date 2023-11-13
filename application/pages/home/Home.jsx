917a4365292352cad738de596afb8994cb126d43
Alexey Kinev <rudy@05bit.com>
2023-11-13 11:00:07 +0400

    Redirect from Home page to Streams

--- application/pages/home/Home.jsx
+++ application/pages/home/Home.jsx
@@ -1,23 +1,25 @@
-import { includes } from "@lodash";
-import React, { useEffect } from "react";
+// import { includes } from "@lodash";
+import { useEffect } from "react";
 
-import Alert from "antd/lib/alert";
-import Link from "@/components/general/Link";
+// import Alert from "antd/lib/alert";
+// import Link from "@/components/general/Link";
 import routeWithUserSession from "@/components/router/routeWithUserSession";
-import EmptyState, { EmptyStateHelpMessage } from "@/components/empty-state/EmptyState";
-import DynamicComponent from "@/components/general/DynamicComponent";
-import PlainButton from "@/components/general/PlainButton";
+// import EmptyState, { EmptyStateHelpMessage } from "@/components/empty-state/EmptyState";
+// import DynamicComponent from "@/components/general/DynamicComponent";
+// import PlainButton from "@/components/general/PlainButton";
+import navigateTo from "@/components/router/navigateTo";
 
-import { axios } from "@/services/axios";
-import recordEvent from "@/services/recordEvent";
-import { messages } from "@/services/auth";
-import notification from "@/services/notification";
+// import { axios } from "@/services/axios";
+// import recordEvent from "@/services/recordEvent";
+// import { messages } from "@/services/auth";
+// import notification from "@/services/notification";
 import routes from "@/services/routes";
 
-import { DashboardAndQueryFavoritesList } from "./components/FavoritesList";
+// import { DashboardAndQueryFavoritesList } from "./components/FavoritesList";
 
 import "./Home.less";
 
+/*
 function DeprecatedEmbedFeatureAlert() {
   return (
     <Alert
@@ -92,12 +94,20 @@ export default function Home() {
     </div>
   );
 }
+*/
+
+export default function HomeRedirect() {
+  useEffect(() => {
+    navigateTo("streams");
+  }, []);
+  return null;
+}
 
 routes.register(
   "Home",
   routeWithUserSession({
     path: "/",
-    title: "Redash",
-    render: pageProps => <Home {...pageProps} />,
+    title: "Home",
+    render: (pageProps) => <HomeRedirect {...pageProps} />,
   })
 );
